7410737e059fb5ff6717ae660b973fe7f6dca925
Yuanheng123123 <yuanheng.liu@stud.kslzh.ch>
2025-12-08 15:14:36 +0100

    fixed weird bug

--- Jass.java
+++ Jass.java
@@ -6,5 +6,17 @@
  * Danach sollen alle Karten auf der Konsole ausgegeben werden.
  */
 public class Jass {
-    
+    public static void main(String[] args){
+        Deck deck = new Deck(); //erstell ein neues Deck
+        deck.shuffle(); // mischelt das Deck
+        System.out.println(deck.getCards().length);
+        for (int i = 0; i < 30; i++){ // entfernt die letzten 3o Karten
+            deck.pop(); //  
+            
+        }
+        deck.addCard(new Card(Suit.EICHELN, Rank.ASS)); // fügt das Eicheln Ass hinzu
+        for (Card card : deck.getCards()){
+            System.out.println(card); //drück die verbleibenden Karten
+        }
+    }
 }
