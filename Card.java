a517a566dc7b1e18d869dba337f3074e0bfd02df
Yuanheng123123 <yuanheng.liu@stud.kslzh.ch>
2025-12-08 15:10:17 +0100

    changed toString() order

--- Card.java
+++ Card.java
@@ -24,7 +24,7 @@ public class Card {
     }
 
     public String toString(){
-        return rank + " " + suit;
+        return suit + " " + rank;
     
     }
     public boolean equals(Card other){
